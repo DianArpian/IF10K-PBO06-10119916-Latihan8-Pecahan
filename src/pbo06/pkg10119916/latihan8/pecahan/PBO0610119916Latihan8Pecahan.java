eca0adf5fc86c65b6873df170fc6fde5a8331964
DianArpian <>
2020-10-20 19:20:45 +0700

    Pecahan

---
+++ src/pbo06/pkg10119916/latihan8/pecahan/PBO0610119916Latihan8Pecahan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo06.pkg10119916.latihan8.pecahan;
+
+/**
+ *
+ * @author 
+ * NAMA     : DIAN ARPIAN
+ * KELAS    : IF10K
+ * NIM      : 10119916 
+ * Deskripsi Program : Pecahan
+ */
+public class PBO0610119916Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
